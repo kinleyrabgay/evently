f049a51b003c4f5bd0866cb88de89e3e3b73c6a9
kinleyrabgay <fsd.rabgay@gmail.com>
2023-12-18 17:34:37 +0600

    -header -footer -hero

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { authMiddleware } from '@clerk/nextjs';
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  // ALLOW VISIT WITHOUT LOGIN
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    'api/webhook/strip',
+    'api/uploadthing',
+  ],
+
+  ignoredRoutes: ['/api/webhook/clerk', 'api/webhook/strip', 'api/uploadthing'],
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
